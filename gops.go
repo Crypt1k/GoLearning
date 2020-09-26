ba24c5fbe85f362473dc51b0bc6a9d9a33bf708f
Vitalii Zhydkov <cryptic.kh.ua@gmail.com>
2020-09-26 12:44:04 -0500

    Add gops issue example

---
+++ gops.go
@@ -0,0 +1,30 @@
+/*
+Gops implements this at the moment with code in goprocess.FindAll() that looks like this, in somewhat sketched and reduced form:
+In the real code there's a WaitGroup for coordination, and the found channel gets closed appropriately.
+*/
+func FindAll() []P {
+   pss, err := ps.Processes()
+   [...]
+   found := make(chan P)
+   limitCh := make(chan struct{}, concurrencyProcesses)
+
+   for _, pr := range pss {
+      limitCh <- struct{}{}
+      pr := pr
+      go func() {
+         defer func() { <-limitCh }()
+         [... get a P with some error checking ...]
+         found <- P
+      }()
+   }
+   [...]
+
+   var results []P
+   for p := range found {
+      results = append(results, p)
+   }
+   return results
+}
+/*
+The bug is that the goroutines only receive from limitCh to release their token after sending their result to the unbuffered found channel, while the main code only starts receiving from found after running through the entire loop, and the main code takes the token in the loop and blocks if no tokens are available. So if you have too many processes to go through, you start N goroutines, they all block trying to write to found and don't receive from limitCh, and the main for loop blocks trying to send to limitCh and never reaches the point where it starts receiving from found.
+*/
